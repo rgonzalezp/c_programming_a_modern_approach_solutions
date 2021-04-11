39cbe9e88dcb545ebe2343239b90d9b5afbad40f
Ricardo Gonzalez <rgonzalezp@bitbucket.org>
2021-04-10 21:37:48 -0400

    Reached chapter 5 exercises

--- chapter_2/ex8.c
+++ chapter_2/ex8.c
@@ -2,3 +2,4 @@
 // 1.answer 2.= 3.( 4. 3 5. * 6. q 7. - 8. p 9. * 10. p 11. ) 12. / 13. 3 14. ;
 // 14 tokens
 // answer = ( 3 * q - p * p ) / 3 ;
+// The spaces that are inside the string literal.
---
+++ chapter_5/exercises/ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int i,j,k;
+    i = 1;
+    j = 2;
+    printf("%d",i += j < j-2);
+
+}
